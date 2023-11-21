e920a0448dc00dcf0e2638c8a3c449a2be4f6778
micsza <michal.szafraniuk@gmail.com>
2023-11-21 15:49:01 +0100

    upd

--- core/src/test/scala/MySpec.scala
+++ core/src/test/scala/MySpec.scala
@@ -3,6 +3,6 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.flatspec.AnyFlatSpec
 class MySpec extends AnyFlatSpec with Matchers {
   "This" should "do" in {
-    1 + 2 should be (3)
+    1 + 2 should be (4)
   }
 }
