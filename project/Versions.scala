7c5e95354458ff42871f30ed36086e315e7f3217
scala-steward <scala-steward>
2023-11-24 08:36:55 +0000

    Update circe-core, circe-generic, ... to 0.14.6

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val catsEffect = "3.5.2"
   val http4s = "0.23.21"
   val scalaTest = "3.2.11"
-  val circe = "0.14.5"
-  val kittens        = "3.0.0"
-  val catsTestkit    = "2.1.3"
+  val circe = "0.14.6"
+  val kittens = "3.0.0"
+  val catsTestkit = "2.1.3"
 }
