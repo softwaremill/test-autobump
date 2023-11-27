1732a9c00679a0e3cc42d3ad7e7ca4a0ab70fd1f
scala-steward <scala-steward>
2023-11-27 13:14:11 +0000

    Revert commit(s) 8492432

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,9 +4,9 @@ object Versions {
   val http4s = "0.23.21"
   val scalaTest = "3.2.11"
   val circe = "0.14.6"
-  val kittens = "3.1.0"
+  val kittens = "3.0.0"
   val catsTestkit = "2.1.3"
   val natchez = "0.3.4"
   val natchezHttp4s = "0.5.0"
-  val pureconfig = "0.17.3"
+  val pureconfig     = "0.17.3"
 }
