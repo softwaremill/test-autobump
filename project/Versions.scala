96af0514f00ff8c44da8b69d9bfe5e04eb6061d0
softwaremill-ci <ci@softwaremill.pl>
2024-01-04 01:11:52 +0100

    Update http4s-circe, http4s-dsl, ... to 0.23.25 (#77)

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   val cats = "2.10.0"
   val catsEffect = "3.5.2"
-  val http4s = "0.23.21"
+  val http4s = "0.23.25"
   val scalaTest = "3.2.11"
   val circe = "0.14.6"
   val kittens = "3.1.0"
