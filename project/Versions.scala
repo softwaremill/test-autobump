38c31d3f3b8fe44b59cd352e3f5cc1dc9ccfb5f4
scala-steward <scala-steward>
2024-02-23 00:09:37 +0000

    Update pureconfig-core, pureconfig-ip4s to 0.17.6

--- project/Versions.scala
+++ project/Versions.scala
@@ -8,7 +8,7 @@ object Versions {
   val catsTestkit = "2.1.5"
   val natchez = "0.3.5"
   val natchezHttp4s = "0.5.0"
-  val pureconfig = "0.17.5"
+  val pureconfig = "0.17.6"
   val datadog4s = "0.32.0"
   val log4cats = "2.6.0"
   val log4j2 = "2.23.0"
