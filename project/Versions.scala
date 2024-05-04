35aca5a3000a06115d86e0a50b749f094f6b207d
scala-steward <scala-steward>
2024-05-04 00:09:49 +0000

    Update log4cats-slf4j to 2.7.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val natchezHttp4s = "0.5.0"
   val pureconfig = "0.17.6"
   val datadog4s = "0.32.0"
-  val log4cats = "2.6.0"
+  val log4cats = "2.7.0"
   val log4j2 = "2.23.1"
   val zio = "2.0.22"
 }
