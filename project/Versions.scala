6694bd5f44b901e9d7a3dbbf8d67f00bde77fdae
scala-steward <scala-steward>
2024-10-17 00:12:07 +0000

    Update natchez-datadog, natchez-log to 0.3.7

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val circe = "0.14.10"
   val kittens = "3.4.0"
   val catsTestkit = "2.1.5"
-  val natchez = "0.3.6"
+  val natchez = "0.3.7"
   val natchezHttp4s = "0.6.0"
   val pureconfig = "0.17.7"
   val datadog4s = "0.32.0"
